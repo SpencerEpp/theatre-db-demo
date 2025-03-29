4cbd73cc56a3ba8e075b4317272bb21eb95b3145
SpencerEpp <sepp162@mtroyal.ca>
2025-03-29 16:47:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -118,6 +118,6 @@ def function():
     return jsonify({"success": False, "error": "Unknown function action"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 # END OF DOC
